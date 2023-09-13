9ab2b986976c015afe034b68c887478ad92bd8f8
Utahh <kaw.alveslima@gmail.com>
2023-09-12 21:57:28 -0300

    feat: update to the code security system

--- api/controllers/NivelController.js
+++ api/controllers/NivelController.js
@@ -1,63 +1,67 @@
-const database = require('../models');
+const Services = require('../services/Services')
+const niveisServices = new Services('Niveis')
 
 class NivelController {
-    static async pegaTodosNiveis(req, res) {
-        try {
-            const todosOsNiveis = await database.Niveis.findAll();
-            return res.status(200).json(todosOsNiveis);
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
+  static async pegaTodosOsNiveis(req, res){  
+    try {
+      const todosOsNiveis = await niveisServices.pegaTodosOsRegistros()
+      return res.status(200).json(todosOsNiveis)  
+    } catch (error) {
+      return res.status(500).json(error.message)
     }
+  }
 
-    static async pegaUmNivel(req,res) {
-        const {id} = req.params;
-        try {
-            const umNivel = await database.Niveis.findOne({
-                where: 
-                {id: Number(id)}
-            });
-            return res.status(200).json(umNivel)
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
+  static async pegaNivel(req, res) {  
+    const { id } = req.params
+    try {
+      const nivel = await niveisServices.pegaUmRegistro({ id })
+      return res.status(200).json(nivel)
+    } catch (error) {
+      return res.status(500).json(error.message)
     }
+  }
 
-    static async crieUmNivel(req, res) {
-        const novoNivel= req.body;
-        try {
-            const novoNivelCriado = await database.Niveis.create(novoNivel);
-            return res.status(200).json(novoNivelCriado);
-        } catch (error) {
-            return res.status(500).json(err.message);
-        }
+  static async criaNivel(req, res) {  
+    const novoNivel = req.body
+    try {
+      const novoNivelCriado = await niveisServices.criaRegistro(novoNivel)
+      return res.status(200).json(novoNivelCriado)
+    } catch (error) {
+      return res.status(500).json(error.message)
     }
+  }
 
-    static async atualizarUmaNivel(req, res) {
-        const {id} = req.params;
-        const novaInfo = req.body;
-        try {
-            await database.Nivel.update(novaInfo, {
-                where: {id: Number(id)}
-            });
-            const nivelAtualizado = await database.Niveis.findOne({where: {
-                id: Number(id)}
-            })
-            return res.status(200).json(nivelAtualizado);
-        } catch (error) {
-            return res.status(500).json(err.message);
-        }
+  static async atualizaNivel(req, res) {  
+    const { id } = req.params
+    const novasInfos = req.body
+    try {
+      await niveisServices.atualizaRegistro(novasInfos, id)
+      return res.status(200).json({ mensagem: `id ${id} atualizado` })
+    } catch (error) {
+      return res.status(500).json(error.message)
     }
+  }
 
-    static async apagaNivel(req, res) {
-        const {id} = req.params;
-        try {
-            await database.Niveis.destroy({where: {id: Number(id)}});
-            return res.status(200).json({ message: `id ${id} deletado`});
-        } catch (error) {
-            return res.status(500).json(err.message);
-        }
+  static async apagaNivel(req, res) {  
+    const { id } = req.params
+    try {
+      await niveisServices.apagaRegistro(id)
+      return res.status(200).json({ mensagem: `id ${id} deletado` })
+
+    } catch (error) {
+      return res.status(500).json(error.message)
     }
+  }
+
+  static async restauraNivel(req, res) {  
+    const { id } = req.params
+    try {
+      await niveisServices.restauraRegistro(id)
+      return res.status(200).json({ mensagem: `id ${id} restaurado` })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
 }
 
-module.exports = NivelController;
+module.exports = NivelController
--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -3,10 +3,10 @@ const PessoaController = require('../controllers/NivelController');
 
 const router = Router();
 
-router.get('/niveis', PessoaController.pegaTodosNiveis);
-router.get('/niveis/:id', PessoaController.pegaUmNivel);
-router.post('/niveis', PessoaController.crieUmNivel);
-router.put('/niveis/:id', PessoaController.atualizarUmaNivel);
+router.get('/niveis', PessoaController.pegaTodosOsNiveis);
+router.get('/niveis/:id', PessoaController.pegaNivel);
+router.post('/niveis', PessoaController.criaNivel);
+router.put('/niveis/:id', PessoaController.atualizaNivel);
 router.delete('/niveis/:id', PessoaController.apagaNivel);
 
 module.exports = router;
